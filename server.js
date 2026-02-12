313f1682d833bd71d37789ef09443e1fc77620f1
BoyLegend808 <ugwunekejohn5@gmail.com>
2026-02-12 03:13:59 +0100

    Add server and package files for Render

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const dns = require("node:dns");
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
+const dns = require('node:dns');
+if (!process.env.RENDER) {
+    dns.setServers(['8.8.8.8', '1.1.1.1']);
+}
 
 const express = require("express");
 const cors = require("cors");
